9883529eac89950f957f716801da858a8490e7b3
Daniil Teterin <danteterin@mail.ru>
2021-07-19 20:15:38 +0300

    some quality of life improvements

---
+++ Src/Notion.Client.Extensions/DatabasesClientExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Notion.Client.Extensions
+{
+    public static class DatabasesClientExtensions
+    {
+        public static async Task QueryToEndAsync(this IDatabasesClient databasesClient,
+            string databaseId, DatabasesQueryParameters databasesQueryParameters, Action<List<Page>> onPartReceived)
+        {
+            databasesQueryParameters = databasesQueryParameters.CreateCopy();
+
+            while (true)
+            {
+                var paginatedList = await databasesClient.QueryAsync(databaseId, databasesQueryParameters);
+                onPartReceived.Invoke(paginatedList.Results);
+
+                if (!paginatedList.HasMore) break;
+
+                databasesQueryParameters.StartCursor = paginatedList.NextCursor;
+            }
+        }
+    }
+}
---
+++ Src/Notion.Client.Extensions/TitleRelatedExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Notion.Client.Extensions
+{
+    public static class TitleRelatedExtensions
+    {
+        public static KeyValuePair<string, PropertyValue> GetTitleProperty(this Page page)
+        {
+            return page.Properties.First(x =>
+                x.Value.Type == PropertyValueType.Title);
+        }
+
+        /// <summary>
+        /// Appends all PlaintText of TitlePropertyValue sub elements
+        /// </summary>
+        public static string GetAllPlainText(this TitlePropertyValue titlePropertyValue)
+        {
+            // to save some efficiency with most common case
+            if (titlePropertyValue.Title.Count == 1)
+                return titlePropertyValue.Title[0].PlainText;
+
+            // in other case append all title sub elements into one string
+            var str = new StringBuilder();
+            foreach (var richTextBase in titlePropertyValue.Title)
+                str.Append(richTextBase.PlainText);
+
+            return str.ToString();
+        }
+    }
+}
